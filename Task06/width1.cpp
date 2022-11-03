a4dac0e3dffad539a7436fa19e63f6cd7670a659
Sviatoslav <ecrprague.asi@gmail.com>
2022-11-04 00:54:22 +0300

    Task06 Done

---
+++ Task06/width1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	long pop1 = 4789426, pop2 = 274124, pop3 = 9761;
+	cout << "City: " << "Population: " << endl
+		<< "Moscow " << pop1 << endl
+		<< "Kirov " << pop2 << endl
+		<< "Ugrumovka " << pop3 << endl;
+	return 0;
+}
